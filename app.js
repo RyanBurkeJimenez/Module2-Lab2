affc580c1691a343a4c984fcf77eb7994f042fa4
Ryanmkbj <Ryanburkejimenez@gmail.com>
2023-09-10 20:13:56 -0700

    Updates to app.js + exercises 2 & 3

--- app.js
+++ app.js
@@ -15,17 +15,35 @@ app.set('views', path.join(__dirname, 'views'));
 console.log('views', path.join(__dirname, 'views'));
 
 const books =[];
-
+// Home webpage when LocalHost 4000 is searched in browser
 app.get("/", (req, res) => {
     res.render("books", { books });
 });
 
+// Push new book into array, pops out of stack in books.ejs function
 app.post("/addBook", (req, res) => {
     const {title, author, publicationYear} = req.body;
     books.push({title, author, publicationYear});
     res.redirect("/");
 });
-////  Write your code here
+
+//Constructor for User object for "create user" function
+function User(name, age, email) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+const users =[];
+  app.get("/users", (req, res) => {
+        res.render("users");
+  });
+//POST function to create user and send User object info to userInfo.ejs for display
+  app.post("/createUser", (req, res) => {
+    const { name, age, email } = req.body;
+    const user = new User(name, age, email);
+    const { name: userName, age: userAge, email: userEmail } = user;
+    res.render("userInfo", { userName, userAge, userEmail });
+  });
 // Start the server on port 4000,
 // Note we are advertising the service on port number 4000 and not 3000 this time
 var port = 4000
