be3dce77ea19c5430f8d7ff9555e232d43d23f5f
Ryanmkbj <Ryanburkejimenez@gmail.com>
2023-09-10 18:13:11 -0700

    Added new package.json file + edited books.ejs

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-//Import required modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require ('path');
+// Import the required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-//create instance of express
+// Create an instance of express
 const app = express();
 
-//body-parser used to parse the incoming request bodies
-app.use(bodyParser.urlencoded({extended: false}));
+// We use the 'body-parser' middleware to parse the incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//set view engine to ejs
+// Set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 console.log('views', path.join(__dirname, 'views'));
 
-//create a route for the home page
-//the GET route for the form
-app.get('/', (req,res)=> {
-    //Render the form and pass in the current student data
-    res.render('index.ejs');
+const books =[];
+
+app.get("/", (req, res) => {
+    res.render("books", { books });
 });
 
-//create a route for user to enter the numbers
-app.post("/calculate", (req, res) => {
-    const {num1, num2} = req.body;
-    const sum = Number(num1) + Number(num2);
-    const difference = Number(num1) - Number(num2);
-    const product = Number(num1) * Number(num2);
-    const quotient = Number(num1) / Number(num2); 
-    res.render("result", {sum, difference, product, quotient});
+app.post("/addBook", (req, res) => {
+    const {title, author, publicationYear} = req.body;
+    books.push({title, author, publicationYear});
+    res.redirect("/");
 });
-
-//Start the server on port 4000
-var port = 4000;
-
+////  Write your code here
+// Start the server on port 4000,
+// Note we are advertising the service on port number 4000 and not 3000 this time
+var port = 4000
+// NOTE
+// the quotes are replaced by back ticks ` next to key caps 1
 app.listen(port, () => {
-console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`);
+  });
